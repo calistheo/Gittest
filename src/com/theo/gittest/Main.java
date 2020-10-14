f9872b6cfb6013d3ef7f6eb189cf0d045c8080e7
calistheo <theo-ruprecht@gmx.de>
2020-10-14 18:56:40 +0200

    add message

--- src/com/theo/gittest/Main.java
+++ src/com/theo/gittest/Main.java
@@ -1,10 +1,13 @@
 package com.theo.gittest;
 
+import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
+
 public class Main {
 
 	public static void main(String[] args) {
 	System.out.println("Test");
 	System.out.println("Hallo");
+		System.out.println("NEIN!!");
 
 	}
 
